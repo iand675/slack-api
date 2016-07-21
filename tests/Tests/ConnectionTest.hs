4f21e558ca6d626e59ce7d07346f46c7a76b2be5
Alex Sayers <alex.sayers@gmail.com>
2016-07-21 16:49:16 +0900

    Fix tests

--- tests/Tests/ConnectionTest.hs
+++ tests/Tests/ConnectionTest.hs
@@ -1,26 +1,37 @@
+
 module Tests.ConnectionTest (main) where
 
-import Web.Slack
+import Control.Monad.IO.Class
+import Data.Maybe
 import System.Environment (lookupEnv)
 import System.Exit
-import System.IO.Unsafe
-
-myConfig :: String -> SlackConfig
-myConfig apiToken = SlackConfig
-         { _slackApiToken = apiToken -- Specify your API token here
-         }
-
-connectBot :: SlackBot ()
-connectBot Hello = unsafePerformIO exitSuccess
-connectBot _ = return ()
-
+import Web.Slack
+import qualified Web.Slack.Handle as H
 
 main :: IO ()
 main = do
-  apiToken <- lookupEnv "SLACK_API_TOKEN"
-  case apiToken of
-    Nothing -> exitFailure
-    Just token ->  runBot (myConfig token) connectBot ()
+    let conf = mkConfig
+    runSlack conf inertBot1
+    H.withSlackHandle conf inertBot2
+    runBot conf inertBot3 ()
 
+mkConfig :: IO SlackConfig
+mkConfig = do
+    x <- lookupEnv "SLACK_API_TOKEN"
+    let apiToken = fromMaybe (error "SLACK_API_TOKEN not set") x
+    return SlackConfig{ _slackApiToken = apiToken }
 
+inertBot1 :: Slack ()
+inertBot1 = do
+    e <- getNextEvent
+    liftIO $ print e
 
+inertBot2 :: H.SlackHandle -> IO ()
+inertBot2 h = do
+    e <- H.getNextEvent h
+    print e
+
+inertBot3 :: SlackBot ()
+inertBot3 e = do
+    liftIO $ print e
+    liftIO exitSuccess
