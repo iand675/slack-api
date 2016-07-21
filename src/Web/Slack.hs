3624fda95b1d2de9093b3796a34a25e9117671c2
Alex Sayers <alex.sayers@gmail.com>
2016-07-21 16:45:36 +0900

    Expose handle-based API

--- src/Web/Slack.hs
+++ src/Web/Slack.hs
@@ -1,12 +1,3 @@
-{-# LANGUAGE DataKinds                  #-}
-{-# LANGUAGE GADTs                      #-}
-{-# LANGUAGE LambdaCase                 #-}
-{-# LANGUAGE NoMonomorphismRestriction  #-}
-{-# LANGUAGE OverloadedStrings          #-}
-
-{-# LANGUAGE ScopedTypeVariables        #-}
-{-# LANGUAGE TypeFamilies               #-}
-{-# LANGUAGE TypeSynonymInstances       #-}
 ------------------------------------------
 -- |
 -- This module exposes functionality to write bots which responds
@@ -30,103 +21,27 @@
 -- > main :: IO ()
 -- > main = runBot myConfig echoBot ()
 --
-module Web.Slack ( runBot
-                 -- Re-exports
-                 , Slack(..)
-                 , SlackBot
-                 , SlackState(..)
-                 , userState
-                 , session
-                 , module Web.Slack.Types
-                 , module Web.Slack.Config
-                 ) where
+module Web.Slack
+    ( MonadSlack
+    , Slack(..)
+    , runSlack
 
-import           Control.Lens
-import Control.Monad (forever, unless)
-import qualified Control.Monad.State        as S
-import           Control.Monad.Trans
-import           Data.Aeson.Lens
-import qualified Data.ByteString.Lazy       as B
-import qualified Data.ByteString.Lazy.Char8 as BC
-import qualified Data.Text                  as T
-import qualified Network.Socket             as S
-import qualified Network.URI                as URI
-import qualified Network.WebSockets         as WS
-import           Network.Wreq
+      -- * Monadic API
+    , getNextEvent
+    , getSession
+    , getConfig
+    , sendPing
+    , sendMessage
+    , sendRichMessage
 
-import           Data.Aeson hiding  (eitherDecode)
-import           Data.Aeson.Coerce
+      -- * Types
+    , SlackConfig(..)
+    , SlackSession(..)
+    , Event(..)
+    , module Web.Slack.Types
+    ) where
 
-import           Web.Slack.Config
-import           Web.Slack.State
-import           Web.Slack.Types
-import           Wuss
-
--- | Run a `SlackBot`. The supplied bot will respond to all events sent by
--- the Slack RTM API.
---
--- Be warned that this function will throw an `IOError` if the connection
--- to the Slack API fails.
-runBot :: forall s . SlackConfig -> SlackBot s -> s -> IO ()
-runBot conf bot start = do
-  r <- get rtmStartUrl
-  let Just (BoolPrim ok) = r ^? responseBody . key "ok"  . _Primitive
-  unless ok (do
-    putStrLn "Unable to connect"
-    ioError . userError . T.unpack $ r ^. responseBody . key "error" . _String)
-  let Just url = r ^? responseBody . key "url" . _String
-  (sessionInfo :: SlackSession) <-
-    case eitherDecode fixRetentionType (r ^. responseBody) of
-      Left e -> print (r ^. responseBody) >> (ioError . userError $ e)
-      Right res -> return res
-  let partialState :: Metainfo -> SlackState s
-      partialState metainfo = SlackState metainfo sessionInfo start conf
-  putStrLn "rtm.start call successful"
-  case parseWebSocketUrl (T.unpack url) of
-    Just (host, path) ->
-      runSecureClient host port path (mkBot partialState bot)
-    Nothing -> error $ "Couldn't parse WebSockets URL: " ++ T.unpack url
-  where
-    port = 443
-    rtmStartUrl :: String
-    rtmStartUrl = "https://slack.com/api/rtm.start?token="
-                    ++ (conf ^. slackApiToken)
-    parseWebSocketUrl :: String -> Maybe (String, String)
-    parseWebSocketUrl url = do
-      uri  <- URI.parseURI url
-      name <- URI.uriRegName <$> URI.uriAuthority uri
-      return (name, URI.uriPath uri)
-    -- correct "1" to 1 for retention_type keys.
-    -- the json slack hands out doesn't adhere to types.
-    fixRetentionType = ((key "team" . key "prefs" . key "retention_type") `over` asNumber)
-      . ((key "team" . key "prefs" . key "group_retention_type") `over` asNumber)
-      . ((key "team" . key "prefs" . key "dm_retention_type") `over` asNumber)
-
-mkBot :: (Metainfo -> SlackState s) -> SlackBot s -> WS.ClientApp ()
-mkBot partialState bot conn = do
-    let initMeta = Meta conn 0
-    WS.forkPingThread conn 10
-    botLoop (partialState initMeta) bot
-
-botLoop :: forall s . SlackState s -> SlackBot s -> IO ()
-botLoop st f =
-  () <$ (flip S.runStateT st  . runSlack $ forever loop)
-  where
-    loop :: Slack s ()
-    loop = do
-      conn <- use connection
-      raw <- liftIO $ WS.receiveData conn
-      let (msg :: Either String Event) = eitherDecode id raw
-      case msg of
-        Left e -> do
-                    liftIO $ BC.putStrLn raw
-                    liftIO $ putStrLn e
-                    liftIO . putStrLn $ "Please report this failure to the github issue tracker"
-        Right event@(UnknownEvent e) -> do
-                    liftIO . print $ e
-                    liftIO . putStrLn $ "Failed to parse to a known event"
-                    liftIO . putStrLn $ "Please report this failure to the github issue tracker"
-                    -- Still handle the event if a user wants to
-                    f event
-        Right event -> f event
+import Control.Monad.State
+import Web.Slack.Monad
+import Web.Slack.Types
 
---
+++ src/Web/Slack/Handle.hs
@@ -0,0 +1,141 @@
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleContexts #-}
+
+-- | The handle-based API. You may find the monadic API (found in
+-- "Web.Slack.Monad") more convenient.
+--
+-- > main :: IO ()
+-- > main = withSlackHandle myConfig echobot
+-- >
+-- > echobot :: SlackHandle -> IO ()
+-- > echobot h = forever $ getNextEvent h >>= \case
+-- >     Message cid _ msg _ _ _ -> sendMessage cid msg h
+-- >     _ -> return ()
+--
+-- This style is typically more verbose than code written with the 'Slack'
+-- monad, but it is more flexible. For instance, consider forking multiple
+-- threads which share a single handle.
+--
+module Web.Slack.Handle
+    ( SlackHandle
+    , withSlackHandle
+
+      -- * Handle-based API
+    , getNextEvent
+    , getSession
+    , getConfig
+    , sendPing
+    , sendMessage
+    , sendRichMessage
+
+      -- * Types
+    , SlackConfig(..)
+    , SlackSession(..)
+    , Event(..)
+    , module Web.Slack.Types
+    ) where
+
+import Control.Error
+import Control.Monad.Except
+import Data.Aeson
+import qualified Data.ByteString.Lazy as BL
+import Data.IORef
+import Data.Monoid
+import qualified Data.Text as T
+import Data.Time.Clock.POSIX
+import qualified Network.Socket as S
+import qualified Network.URI as URI
+import qualified Network.WebSockets as WS
+import Web.Slack.Types
+import Web.Slack.WebAPI
+import Wuss
+
+data SlackHandle = SlackHandle
+    { _shConfig     :: SlackConfig
+    , _shSession    :: SlackSession
+    , _shConnection :: WS.Connection
+    , _shCounter    :: IORef Int
+    }
+
+withSlackHandle :: SlackConfig -> (SlackHandle -> IO a) -> IO a
+withSlackHandle conf fn = fromExceptT (ioError . userError . T.unpack) $ do
+    (url, sessionInfo) <- rtm_start conf
+    (host, path) <- parseWebSocketUrl url
+    liftIO $ runSecureClient host 443 path $ \conn -> do
+        freshCounter <- newIORef 0
+        let h = SlackHandle
+              { _shConfig = conf
+              , _shConnection = conn
+              , _shSession = sessionInfo
+              , _shCounter = freshCounter
+              }
+        WS.forkPingThread conn 10
+        fn h
+
+parseWebSocketUrl :: Monad m => T.Text -> ExceptT T.Text m (String, String)
+parseWebSocketUrl url = do
+    uri  <- URI.parseURI (T.unpack url) ?? ("Couldn't parse WebSockets URL: " <> url)
+    auth <- URI.uriAuthority uri ?? ("No authority: " <> url)
+    return (URI.uriRegName auth, URI.uriPath uri)
+
+getConfig :: SlackHandle -> SlackConfig
+getConfig = _shConfig
+
+getSession :: SlackHandle -> SlackSession
+getSession = _shSession
+
+getNextEvent :: SlackHandle -> IO Event
+getNextEvent h@SlackHandle{..} = do
+    raw <- WS.receiveData _shConnection
+    case eitherDecode raw of
+        Left e -> do
+            putStrLn $ unlines
+                [ show raw
+                , e
+                , "Please report this failure to the github issue tracker"
+                ]
+            getNextEvent h
+        Right event@(UnknownEvent val) -> do
+            putStrLn $ unlines
+                [ show val
+                , "Failed to parse to a known event"
+                , "Please report this failure to the github issue tracker"
+                ]
+            return event
+        Right event ->
+            return event
+
+nextMessageId :: SlackHandle -> IO Int
+nextMessageId SlackHandle{..} = do
+    liftIO $ modifyIORef _shCounter (+1)
+    liftIO $ readIORef _shCounter
+
+sendMessage :: SlackHandle -> ChannelId -> T.Text -> IO ()
+sendMessage h@SlackHandle{..} cid message = do
+    uid <- nextMessageId h
+    let payload = MessagePayload uid "message" cid message
+    WS.sendTextData _shConnection (encode payload)
+
+sendPing :: SlackHandle -> IO ()
+sendPing h@SlackHandle{..} = do
+    uid <- nextMessageId h
+    now <- round <$> getPOSIXTime
+    let payload = PingPayload uid "ping" now
+    WS.sendTextData _shConnection (encode payload)
+
+sendRichMessage
+    :: SlackHandle -> ChannelId -> T.Text -> [Attachment] -> IO (Either T.Text ())
+sendRichMessage h cid msg as =
+    runExceptT $ chat_postMessage (getConfig h) cid msg as
+
+-------------------------------------------------------------------------------
+-- Helpers
+
+fromExceptT :: Monad m => (e -> m a) -> ExceptT e m a -> m a
+fromExceptT handle act = runExceptT act >>= either handle return
+
+-- crashOnError :: MonadIO m => ExceptT T.Text m a -> m a
+-- crashOnError x = runExceptT x >>= \case
+--     Left  e -> liftIO $ ioError $ userError (T.unpack e)
+--     Right a -> return a
